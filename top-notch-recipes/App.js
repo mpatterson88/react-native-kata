87c777d48614757e064538c4fdaa1d7380bffa2a
Michael Patterson <mpatterson@pillartechnology.com>
2018-05-10 13:55:34 -0500

    Working through the react native tutorial.

--- top-notch-recipes/App.js
+++ top-notch-recipes/App.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Hello Fruit!</Text>
+          <Image source={pic} style={{width: 193, height: 110}}></Image>
+          <Greeting name="Tyler"></Greeting>
+          <Greeting name="Matt"></Greeting>
+          <Greeting name="Steve"></Greeting>
       </View>
     );
   }
+}
+
+class Greeting extends React.Component {
+    render() {
+        let txt ="Hello " + this.props.name + "!";
+        return (
+            <Blink text={ txt }></Blink>
+        );
+    }
+}
+
+class Blink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    setInterval(() => {
+        this.setState(previousState => {
+            return {isShowingText: !previousState.isShowingText };
+        });
+    }, 1000);
+  }
+    render() {
+        let display = this.state.isShowingText ? this.props.text : ' ';
+        return (
+            <Text>{display}</Text>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
